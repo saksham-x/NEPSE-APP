045b97129e88efbe6559d0ea02a6eea79ad296cf
saksham-x <sakshampokharel2002@gmail.com>
2025-05-31 14:21:29 +0545

    Second Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import "./App.css";
-
 function App() {
-  return <>HELLO SAKXY BRO</>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-red-500">
+        Hi there, this is a React app with TypeScript and Vite!
+      </h1>
+    </>
+  );
 }
 
 export default App;
