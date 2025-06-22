39ed79e6e059eb53655141a80557e8cfd6bc7b91
saksham-x <sakshampokharel2002@gmail.com>
2025-06-22 23:32:05 +0545

    Minor fixes

--- src/pages/dashboard/abc.tsx
+++ src/pages/dashboard/abc.tsx
@@ -1,7 +1,12 @@
 type Props = {};
 
 const abc = (props: Props) => {
-  return <div>THis is just to mantain the streak</div>;
+  return (
+    <>
+      <div>THis is just to mantain the streak</div>
+      <div>This is just to mantain the streak day 2 </div>
+    </>
+  );
 };
 
 export default abc;
